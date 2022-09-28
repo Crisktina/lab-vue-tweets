fe366b8db23b0c31aa48397c4d9127b9a3f8212d
Crisktina <crishidalgolopez@gmail.com>
2022-09-28 17:54:59 +0200

    inicio

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import MyBio from "./components/MyBio.vue"
+</script>
+
+<template>
+  <MyBio 
+  name = "Pepito"
+  age = 35
+  birthplace = "Igualada"
+  currentLocation = "Aqui mismo!"/>
+
+</template>
+
+<style scoped>
+
+</style>
